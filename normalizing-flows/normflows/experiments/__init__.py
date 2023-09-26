5bc1d000f341338310256320a8094998391bbd28
telegraphroad <saba.amiri@outlook.com>
2023-09-26 10:47:41 +0200

    bug fix

--- normalizing-flows/normflows/experiments/__init__.py
+++ normalizing-flows/normflows/experiments/__init__.py
@@ -3,11 +3,11 @@ from . import (
 )
 
 from .flowslib import (
-    make_nice_flow,
-    make_rnvp_flow,
-    make_residual_flow,
-    make_nsp_flow,
-    make_iaf_flow,
-    make_radial_flow,
-    make_planar_flow
+    planar,
+    radial,
+    nice,
+    rnvp,
+    nsp,
+    iaf,
+    residual
 )
